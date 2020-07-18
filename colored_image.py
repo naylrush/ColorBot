b5e4a12ce0871fc824957e64b2af67c66e649c03
Alexey Davletshin <naylrush@gmail.com>
2020-07-18 16:52:28 +0300

    Refactor

---
+++ colored_image.py
@@ -0,0 +1,45 @@
+
+from PIL import Image, ImageDraw
+from random import randint
+import io
+
+
+def random_color():
+    """
+    Returns random color.
+
+    :return: (int, int, int)
+    """
+    return randint(0, 255), randint(0, 255), randint(0, 255)
+
+
+def color_to_html(color):
+    """
+    Convert color to HTML color code.
+
+    :param color: (int, int, int)
+    :return: str
+    """
+    if len(color) != 3:
+        raise Exception('Three color properties required')
+
+    return '#' + ''.join(map(lambda s: format(s, 'x'), color)).upper()
+
+
+def colored_image(color=None):
+    """
+    Generates 500x500 image filled in with a given or random color.
+
+    :return: bytes, str
+    """
+    width, height = 500, 500
+    image = Image.new('RGB', (width, height))
+
+    color = color if color else random_color()
+    ImageDraw.floodfill(image, xy=(0, 0), value=color)
+
+    bytes_buffer = io.BytesIO()
+    image.save(bytes_buffer, format='PNG')
+    bytes_image = bytes_buffer.getvalue()
+
+    return bytes_image, color_to_html(color)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
-from PIL import Image, ImageDraw
-from random import randint
-import io
 import telebot
+import colored_image
 
 
 # Make a bot
@@ -15,45 +13,6 @@ random_button = telebot.types.KeyboardButton('Random')
 markup.row(random_button)
 
 
-def random_color():
-    """
-    Returns random color.
-
-    :return: (int, int, int, int)
-    """
-    return randint(0, 255), randint(0, 255), randint(0, 255)
-
-
-def color_to_html_code(color):
-    """
-    Convert color to HTML code.
-
-    :param color: [int]
-    :return: str
-    """
-    if len(color) != 3:
-        raise Exception('Three color properties required')
-
-    return '#' + ''.join(map(lambda x: x[2:], map(hex, color))).upper()
-
-
-def generate_colored_image(color=None):
-    """
-    Generates 500x500 image filled in with a given or random color.
-
-    :return: bytes, str
-    """
-    width, height = 500, 500
-    image = Image.new('RGB', (width, height))
-
-    color = color if color else random_color()
-    ImageDraw.floodfill(image, xy=(0, 0), value=color)
-
-    bytes_buffer = io.BytesIO()
-    image.save(bytes_buffer, format='PNG')
-    bytes_image = bytes_buffer.getvalue()
-
-    return bytes_image, color_to_html_code(color)
 
 
 @bot.message_handler(content_types=['text'])
@@ -66,8 +25,9 @@ def send_message(message):
     :return: None
     """
     if message.text == 'Random':
-        image, html_code = generate_colored_image()
-        bot.send_photo(message.chat.id, caption=html_code, photo=image)
+        image, html = colored_image.colored_image()
+        bot.send_photo(message.chat.id, photo=image, caption=html)
+        bot.send_photo(message.chat.id, photo=image, caption=html_code)
     else:
         bot.send_message(message.chat.id, text='Choose a button:', reply_markup=markup)
 
